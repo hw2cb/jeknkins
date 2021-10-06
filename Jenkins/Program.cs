e66fcc9ca4c0aaa66481f6c348a28d0b10150abf
hw2cb <dim5.dim5.960@mail.ru>
2021-10-06 17:18:21 +0400

    init

---
+++ Jenkins/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Jenkins
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var animal = new Number();
+            Console.WriteLine(animal.GetNumber(4));
+            Console.ReadLine();
+        }
+    }
+}
