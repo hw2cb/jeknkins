773682bcf4a815a94f33d1354d107dd121150110
hw2cb <dim5.dim5.960@mail.ru>
2021-10-11 23:53:53 +0400

    fixs

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 MD C:\Results\bin\Jenkins
 MD C:\Results\bin\JenkinsTest
 cd C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\
-dotnet restore
+dotnet restore C:\Users\Administrator\AppData\Local\Jenkins\.jenkins\workspace\MyJob
 msbuild.exe C:\Users\Administrator\AppData\Local\Jenkins\.jenkins\workspace\MyJob\Jenkins /t:Build /p:OutputPath="C:\Results\bin\Jenkins"
 msbuild.exe C:\Users\Administrator\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsMsTest /t:Build /p:OutputPath="C:\Results\bin\JenkinsTest"
