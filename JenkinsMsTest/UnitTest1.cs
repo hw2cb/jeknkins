9eecc80c9d0c5f17ad0b361d4c1abecfbd08dba3
hw2cb <dim5.dim5.960@mail.ru>
2021-10-11 19:58:48 +0400

    edit

--- JenkinsMsTest/UnitTest1.cs
+++ JenkinsMsTest/UnitTest1.cs
@@ -9,7 +9,7 @@ namespace JenkinsMsTest
         public void TestMethod1()
         {
             Number animal = new Number();
-            int expectedNumber = 4;
+            int expectedNumber = 7;
             int number = animal.GetNumber(expectedNumber);
             Assert.AreEqual(expectedNumber, number);
         }
