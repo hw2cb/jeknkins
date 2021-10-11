adf2fefb4cfede72e503fba5dba47c0a99d65c23
hw2cb <dim5.dim5.960@mail.ru>
2021-10-11 23:41:58 +0400

    build ready, jenkins ready

--- build.bat
+++ build.bat
@@ -1,2 +1,5 @@
+MD C:\Results\bin\Jenkins
+MD C:\Results\bin\JenkinsTest
 cd C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\MSBuild\Current\Bin\
-msbuild.exe C:\Repository\jeknkins"
+msbuild.exe C:\Users\Administrator\AppData\Local\Jenkins\.jenkins\workspace\MyJob\Jenkins /t:Build /p:OutputPath="C:\Results\bin\Jenkins"
+msbuild.exe C:\Users\Administrator\AppData\Local\Jenkins\.jenkins\workspace\MyJob\JenkinsMsTest /t:Build /p:OutputPath="C:\Results\bin\JenkinsTest"
